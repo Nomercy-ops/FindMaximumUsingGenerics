7b9031df935377920ae2776a98603a817fddd93d
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-11 23:38:32 +0530

    Refactor: all the 3 to

--- FindMaximum.java
+++ FindMaximum.java
@@ -8,110 +8,52 @@
  */
 package com.bridgelabz.generics;
 
+
 public class FindMaximum {
     
- /**
-  * UC-1 Here find maximum method returns maximum Number.using a wrapper class Integer and comparing it with compareTo method.
-  * @param number1
-  * @param number2
-  * @param number3
-  * 
-  */
-
-    public void findMaxInteger(Integer number1, Integer number2, Integer number3) {
-        Integer max = number1;
-        if (number2.compareTo(max) > 0) {
-            max = number2;
+/**
+ * Refactor-1 making one One Generic Method and finding the maximum ensuring
+ * generic is extended to comparable.
+ *
+ *
+     * @param <E>
+     * @param firstInput
+     * @param secondInput
+     * @param thirdInput
+ */
+    
+    public <E extends Comparable> void findMax(E firstInput, E secondInput, E thirdInput) {
+        E max = firstInput;
+        if (secondInput.compareTo(max) > 0) {
+            max = secondInput;
         }
-        if (number3.compareTo(max) > 0) {
-            max = number3;
+        if (thirdInput.compareTo(max) > 0) {
+            max = thirdInput;
         }
+        printMax(max);
 
-        printMaxInteger(max);
     }
-    
-    /**
-     *  method for printing max Integer value. 
-     * @param max 
-     */
-      
-    public void printMaxInteger(Integer max){
-        System.out.println("Maximum Integer Number is  : " + max);
-    }
-    
- /**
-  * UC-2   findMaximum method returns maximum Double Number.using a wrapper class Double 
-  * and comparing it with compareTo method.
-  * @param number1
-  * @param number2
-  * @param number3
-  * 
-  */
-    
-    public void findMaximumDouble(Double number1, Double number2, Double number3) {
-         Double max = number1;
-       if(number2.compareTo(max)> 0){
-           max = number2;
-       }
-       if(number3.compareTo(max)>0){
-           max = number3;
-       }
-        printMaxDouble(max);
-    }
-     
-    /**
-     *  method for printing max Double value. 
-     * @param max 
-     */
-     
-    private void printMaxDouble(Double max) {
-     System.out.println("Maximum Double Number is : " + max);
-    }
-     
- /**
-  * UC-2   findMaximum method returns maximum String.
-  * and comparing it with compareTo method to find max.
-  * 
-     * @param fruitA
-     * @param fruitB
-     * @param fruitC
-  */
-     
-    public void findMaximumString(String fruitA, String fruitB, String fruitC) {
-         String max = fruitA;
-       if(fruitB.compareTo(max)> 0){
-           max = fruitB;
-       }
-       if(fruitC.compareTo(max)>0){
-           max = fruitC;
-       }
-        printString(max);
-    }
-    
-    
-    /**
-     *  method for printing max String. 
-     * @param max 
-     */
-    private void printString(String max) {
-      System.out.println("Maximum String Fruit is :  " + max); 
+
+    private <E> void printMax(E max) {
+        System.out.println("Maximum String Fruit is :  " + max);
     }
 
     /**
-     * This is the main method for passing the Integer values on find max method .  
-     * @param args 
+     * This is the main method for passing the Integer values on find max method
+     * .it is also passing double and String value 0n find max method
+     *
+     * @param args
      */
-    
     public static void main(String[] args) {
-        
+
         // creating class object to call find maximum method.
         FindMaximum findmaximum = new FindMaximum();
-        Integer number1 = 10, number2 = 30,number3 = 20;
-        findmaximum.findMaxInteger(number1, number2, number3);
-        Double num1 = 3.5,num2 = 10.3, num3 = 4.5;
-        findmaximum.findMaximumDouble(num1, num2, num3);
+        Integer number1 = 10, number2 = 30, number3 = 20;
+        findmaximum.findMax(number1, number2, number3);
+        Double num1 = 3.5, num2 = 10.3, num3 = 4.5;
+        findmaximum.findMax(num1, num2, num3);
         String fruits1 = "Apple", fruits2 = "Peach", fruits3 = "Banana";
-        findmaximum.findMaximumString(fruits1, fruits2, fruits3);
+        findmaximum.findMax(fruits1, fruits2, fruits3);
     }
 
 }
