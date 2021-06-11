852ebdf5187c5f2c84186ab1ba209fa49cb2372b
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-11 22:01:07 +0530

    Added: method for finding maxium between three Double number and print it

--- FindMaximum.java
+++ FindMaximum.java
@@ -11,11 +11,11 @@ package com.bridgelabz.generics;
 public class FindMaximum {
     
  /**
-  * UC-1 to find maximum method returns maximum value.using a wrapper class Integer and comparing it with compareTo method.
+  * UC-1 Here find maximum method returns maximum Number.using a wrapper class Integer and comparing it with compareTo method.
   * @param number1
   * @param number2
   * @param number3
-  * @return max Integer value
+  * 
   */
 
     public void findMaxInteger(Integer number1, Integer number2, Integer number3) {
@@ -34,10 +34,38 @@ public class FindMaximum {
      *  method for printing max Integer value. 
      * @param max 
      */
-    
-    
+      
     public void printMaxInteger(Integer max){
-        System.out.println("Largest Integer Number is " + max);
+        System.out.println("Maximum Integer Number is " + max);
+    }
+    
+ /**
+  * UC-2   findMaximum method returns maximum Double Number.using a wrapper class Double 
+  * and comparing it with compareTo method.
+  * @param number1
+  * @param number2
+  * @param number3
+  * 
+  */
+    
+     public void findMaximumDouble(Double number1, Double number2, Double number3) {
+         Double max = number1;
+       if(number2.compareTo(max)> 0){
+           max = number2;
+       }
+       if(number3.compareTo(max)>0){
+           max = number3;
+       }
+        printMaxDouble(max);
+    }
+     
+    /**
+     *  method for printing max Double value. 
+     * @param max 
+     */
+     
+     private void printMaxDouble(Double max) {
+     System.out.println("Maximum Double Number is " + max);
     }
 
     /**
@@ -48,9 +76,11 @@ public class FindMaximum {
     public static void main(String[] args) {
         
         // creating class object to call find maximum method.
-       FindMaximum findmaximum = new FindMaximum();
+        FindMaximum findmaximum = new FindMaximum();
         Integer number1 = 10, number2 = 30,number3 = 20;
         findmaximum.findMaxInteger(number1, number2, number3);
+        Double num1 = 3.5,num2 = 10.3, num3 = 4.5;
+        findmaximum.findMaximumDouble(num1, num2, num3);
        
     }
 
