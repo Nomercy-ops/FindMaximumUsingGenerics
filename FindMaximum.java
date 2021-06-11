ad1f2fbfca53a497a226877f375464ebf52ed571
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-12 00:56:45 +0530

    Refactor: previous code and make the class generic which takes three variables and print max

--- FindMaximum.java
+++ FindMaximum.java
@@ -8,39 +8,46 @@
  */
 package com.bridgelabz.generics;
 
-
-public class FindMaximum {
-    
 /**
- * Refactor-1 making one One Generic Method and finding the maximum ensuring
+ * Refactor-2 making one One Generic Method and finding the maximum ensuring
  * generic is extended to comparable.
- *
- *
-     * @param <E>
-     * @param firstInput
-     * @param secondInput
-     * @param thirdInput
+ * @param <E>
  */
+
+public class FindMaximum<E extends Comparable<E>>{
     
-    public <E extends Comparable> void findMax(E firstInput, E secondInput, E thirdInput) {
-        E max = firstInput;
-        if (secondInput.compareTo(max) > 0) {
-            max = secondInput;
-        }
-        if (thirdInput.compareTo(max) > 0) {
-            max = thirdInput;
-        }
-        printMax(max);
+    E input1,input2,input3;
 
+    //parameterized constructor of a class
+    public FindMaximum(E input1, E input2, E input3) {
+        this.input1 = input1;
+        this.input2 = input2;
+        this.input3 = input3;
     }
-	
-  /**
-	* Method for printing all the methods values
-	* @param max
-	*/
+    
+    public E findMax(){
+        return FindMaximum.findMax(input1,input2,input3);
+    }
+  
+    
+    public static  <E extends Comparable<E>> E findMax(E input1, E input2, E input3) {
+        E max = input1;
+        if (input2.compareTo(max) > 0) {
+            max = input2;
+        }
+        if (input3.compareTo(max) > 0) {
+            max = input3;
+        }
+       printMax(max);
+        return max;
+    }
 
-    private <E> void printMax(E max) {
-        System.out.println("Maximum  is :  " + max);
+  /**
+    *  Method for printing all max values.
+	*/
+	
+    public static <E> void printMax(E max) {
+      System.out.println("Max Value = " + max);
     }
 
     /**
@@ -52,13 +59,14 @@ public class FindMaximum {
     public static void main(String[] args) {
 
         // creating class object to call find maximum method.
-        FindMaximum findmaximum = new FindMaximum();
+        
         Integer number1 = 10, number2 = 30, number3 = 20;
-        findmaximum.findMax(number1, number2, number3);
-        Double num1 = 3.5, num2 = 10.3, num3 = 4.5;
-        findmaximum.findMax(num1, num2, num3);
+        Double doubleNum1 = 3.5, doubleNum2 = 10.3, doubleNum3 = 4.5;
         String fruits1 = "Apple", fruits2 = "Peach", fruits3 = "Banana";
-        findmaximum.findMax(fruits1, fruits2, fruits3);
+        
+        new FindMaximum(number1,number2,number3).findMax();
+        new FindMaximum(doubleNum1,doubleNum2,doubleNum3).findMax();
+        new FindMaximum(fruits1,fruits2,fruits3).findMax();
     }
 
 }
