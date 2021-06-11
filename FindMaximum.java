3f30d269072d94798f02bce42088f599f5d7acaa
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-11 21:47:04 +0530

    Modified: method name for better readability

--- FindMaximum.java
+++ FindMaximum.java
@@ -18,7 +18,7 @@ public class FindMaximum {
   * @return max Integer value
   */
 
-    public void findMax(Integer number1, Integer number2, Integer number3) {
+    public void findMaxInteger(Integer number1, Integer number2, Integer number3) {
         Integer max = number1;
         if (number2.compareTo(max) > 0) {
             max = number2;
@@ -27,7 +27,7 @@ public class FindMaximum {
             max = number3;
         }
 
-        printMax(max);
+        printMaxInteger(max);
     }
     
     /**
@@ -36,7 +36,7 @@ public class FindMaximum {
      */
     
     
-    public void printMax(Integer max){
+    public void printMaxInteger(Integer max){
         System.out.println("Largest Integer Number is " + max);
     }
 
@@ -50,7 +50,7 @@ public class FindMaximum {
         // creating class object to call find maximum method.
        FindMaximum findmaximum = new FindMaximum();
         Integer number1 = 10, number2 = 30,number3 = 20;
-        findmaximum.findMax(number1, number2, number3);
+        findmaximum.findMaxInteger(number1, number2, number3);
        
     }
 
